1dd89e27f2ebfe9f02d2dbd6109fd134c945dcd2
David Dever <david.dever@cox.net>
2014-10-08 01:05:57 +0000

    email validation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionController::TestCase
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get :new
     assert_response :success
