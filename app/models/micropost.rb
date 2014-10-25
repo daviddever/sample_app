778bef6c1e308a17f7b072b1f959fe628783fb20
David Dever <david.dever@cox.net>
2014-10-25 07:01:37 +0000

    Finished feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,16 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate :picture_size
-  
+
+  # Returns microposts from the users being followed by the given user.
+  def Micropost.from_users_followed_by(user)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: user)
+  end
+
   private
-    
+
     def picture_size
       if picture.size > 5.megabytes
         errors.add(:picture, "should be less than 5MB")
